8e8b05d59480d602373dd9750ef3093850c3d532
curso_git <kekinpraxedes@gmail.com>
2026-06-23 17:32:33 -0300

    feat: rotas de stock criadas com sucesso, correção de alguns bugs lógicos

---
+++ sistem/TPV2/Stock/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import StockController
+
+controller = StockController()
+
+urlpatterns = [
+    path('stock/item/create/', controller.createItemStock, name='create_item_stock'),
+    path('stock/return/', controller.returnStockWithItems, name='return_stock'),
+    path('stock/modify/', controller.modifyStock, name='modify_stock'),
+    path('stock/item/update-amount/', controller.updateAmountAndLog, name='update_amount_and_log'),
+    path('stock/delete/', controller.deleteStock, name='delete_stock'),
+    path('stock/item/delete/', controller.deleteStockItem, name='delete_stock_item'),
+]
